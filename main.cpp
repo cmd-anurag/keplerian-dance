5e226de9203be69b16019a191504dc97e804f472
cmd-anurag <anuragkumar.7157@gmail.com>
2025-07-15 20:50:12 +0530

    refactor and camera follow

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 #include<cmath>
 
-constexpr float G = 150.0f;
+constexpr float G = 200.0f;
 
 
 float calculateDistance(sf::Vector2f p1, sf::Vector2f p2)
@@ -18,6 +18,7 @@ sf::Vector2f normalize(sf::Vector2f vector)
     if(magnitude == 0) return {0,0};
     return vector / magnitude;
 }
+
 
 
 struct Body {
@@ -44,14 +45,53 @@ struct Body {
     }
 };
 
+void PhysicsUpdates(std::vector<Body> &bodies, float dt)
+{
+    // physics updates
+    for(int i = 0; i < bodies.size(); ++i) {
+        for(int j = i + 1; j <  bodies.size(); ++j) {
+
+            sf::Vector2f delta = bodies[j].position - bodies[i].position;            
+            float distance = calculateDistance(bodies[j].position, bodies[i].position);
+            float clampedDistance = std::max(distance, 1.0f);
+
+            float forceMagnitude = G * bodies[i].mass * bodies[j].mass / (clampedDistance * clampedDistance);
+            sf::Vector2f forceDirection = normalize(delta);
+
+            sf::Vector2f acc1 = (forceMagnitude / bodies[i].mass) * forceDirection;
+            sf::Vector2f acc2 = (forceMagnitude / bodies[j].mass) * -forceDirection; // equal but opposite accleeration
+
+            bodies[i].acceleration += acc1;
+            bodies[j].acceleration += acc2;
+        }
+    }
+    
+    // update velocity and position + trails
+    for(Body &body: bodies) {
+        body.trail.append(sf::Vertex(body.position, sf::Color::White));
+
+        if(body.trail.getVertexCount() > 1500) {
+            sf::VertexArray newTrail{sf::LineStrip};
+
+            for(size_t i = 1; i < body.trail.getVertexCount(); ++i)
+            {
+                newTrail.append(body.trail[i]);
+            }
+            body.trail = std::move(newTrail);
+        }
+        body.update(dt);
+    }
+}
+
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Sim");
+    sf::View view = window.getDefaultView();
 
     std::vector<Body> bodies;
 
-    Body body1({1150, 500}, 1, 20, sf::Color::Red);         // ORIBITING MASS
-    Body body2({950, 500}, 100000, 30, sf::Color::Blue);       // CENTRAL MASS
+    Body body1({1250, 500}, 20000, 20, sf::Color::Blue);         // ORIBITING MASS
+    Body body2({950, 500}, 90000, 40, sf::Color::Yellow);       // CENTRAL MASS
     
     float orbitalVelocityMagnitude = sqrt(G * body2.mass / calculateDistance(body1.position, body2.position)); // root(Gm/r)
     
@@ -60,7 +100,7 @@ int main() {
     sf::Vector2f tangentialVector = {normalizedRV.y, -normalizedRV.x};
 
     body1.velocity = orbitalVelocityMagnitude * tangentialVector;
-    body1.velocity *= 1.3f; // elliptical
+    // body1.velocity *= 1.4f; // elliptical
 
     bodies.push_back(body1);
     bodies.push_back(body2);
@@ -80,45 +120,24 @@ int main() {
         }
 
 
-        // physics updates
-        for(int i = 0; i < bodies.size(); ++i) {
-            for(int j = i + 1; j <  bodies.size(); ++j) {
-                sf::Vector2f delta = bodies[j].position - bodies[i].position;
-                
-                float squaredDistance = delta.x * delta.x + delta.y * delta.y; // distance formula - (x2 - x1)^2 + (y2 - y1)^2
-                float distance = sqrt(squaredDistance);
-                float clampedDistance = std::max(distance, 5.0f);
+        // Physics
+        PhysicsUpdates(bodies, dt);
 
-                float forceMagnitude = G * bodies[i].mass * bodies[j].mass / (clampedDistance * clampedDistance);
-                sf::Vector2f forceDirection = delta / clampedDistance;
+        window.clear();
 
-                sf::Vector2f acc1 = (forceMagnitude / bodies[i].mass) * forceDirection;
-                sf::Vector2f acc2 = (forceMagnitude / bodies[j].mass) * -forceDirection; // equal but opposite accleeration
+        // barycenter
+        // sf::CircleShape barycenter(10);
+        // barycenter.setFillColor(sf::Color::Red);
+        // sf::Vector2f baryPosition = (bodies[0].position * bodies[0].mass + bodies[1].position * bodies[1].mass) / (bodies[0].mass + bodies[1].mass);
+        // barycenter.setPosition(baryPosition);
+        // barycenter.setOrigin(10, 10);
+        // window.draw(barycenter);
 
-                bodies[i].acceleration += acc1;
-                bodies[j].acceleration += acc2;
-            }
-        }
-
-        for(Body &body: bodies) {
-            
-
-            body.trail.append(sf::Vertex(body.position, sf::Color::White));
-
-            if(body.trail.getVertexCount() > 1500) {
-                sf::VertexArray newTrail{sf::LineStrip};
-
-                for(size_t i = 1; i < body.trail.getVertexCount(); ++i)
-                {
-                    newTrail.append(body.trail[i]);
-                }
-                body.trail = std::move(newTrail);
-            }
-            body.update(dt);
-        }
+        // camera followss orbiting body
+        view.setCenter(bodies[0].position);
+        window.setView(view);
 
         // rendering
-        window.clear();
         for(auto &body : bodies) {
             
             window.draw(body.trail);
@@ -131,6 +150,8 @@ int main() {
         }
         window.setFramerateLimit(60);
         window.display();
+
+        
     }
 
     return 0;
